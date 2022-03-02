1f2492e6ece2d59b0f8dd57984cc428132aa15f1
Yesim Dundar <kuranyesim@gmail.com>
2022-03-02 16:39:55 +0100

    Complete addStock function

--- database/addStock.go
+++ database/addStock.go
@@ -1,21 +1,16 @@
 package database
 
 import (
-	"fmt"
+	"github.com/spf13/cobra"
 )
 
-func AddStock(name string, ticker string) {
-	portfolio := SelectPortfolio(name)
+func AddStock(portfolioName string, ticker string) {
+	portfolio := SelectPortfolio(portfolioName)
 
-	fmt.Println(portfolio)
-	/*
-		addStock, _ := dataBase.Prepare("INSERT INTO stocks (portfolio_id, ticker) VALUES (?, ?)")
+	addStock, queryErr := database.Prepare("INSERT INTO stocks (portfolio_id, ticker) VALUES (?, ?)")
+	defer addStock.Close()
+	cobra.CheckErr(queryErr)
 
-		defer addStock.Close()
-
-		_, insertErr := addStock.Exec(portfolio.Id, ticker)
-
-		cobra.CheckErr(insertErr)
-
-	*/
+	_, insertErr := addStock.Exec(portfolio.Id, ticker)
+	cobra.CheckErr(insertErr)
 }

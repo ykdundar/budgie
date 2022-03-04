1cef13137803ee6c6e9fa9f9b962826934b79acf
Yesim Dundar <kuranyesim@gmail.com>
2022-03-04 03:19:39 +0100

    Rename remove command to delete

--- cmd/stock.go
+++ cmd/stock.go
@@ -39,14 +39,13 @@ var addCmd = &cobra.Command{
 ON CONFLICT(name) DO UPDATE SET phonenumber=excluded.phonenumber;
 */
 
-
 var removeCmd = &cobra.Command{
-	Use:   "remove",
-	Short: "Removes a given stock",
+	Use:   "delete",
+	Short: "Deletes a given stock",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("remove called")
+		database.DeleteStock(portfolio, ticker)
 	},
-	Example: `budgie stock remove
+	Example: `budgie stock delete
 	--portfolio "European Stocks"
 	--ticker "MSFT"
 `,
@@ -64,22 +63,23 @@ year 2`,
 
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("report called")
+
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(stockCmd)
-	stockCmd.AddCommand(addCmd, removeCmd, reportCmd)
+	stockCmd.AddCommand(addCmd, deleteCmd, reportCmd)
 
 	addCmd.PersistentFlags().StringVarP(&portfolio, "portfolio", "p", "", "Portfolio name (required)")
 	addCmd.MarkPersistentFlagRequired("portfolio")
 	addCmd.PersistentFlags().StringVarP(&ticker, "ticker", "s", "", "Company name (required)")
 	addCmd.MarkPersistentFlagRequired("ticker")
 
-	removeCmd.PersistentFlags().StringVarP(&portfolio, "portfolio", "p", "", "Portfolio name (required)")
-	removeCmd.MarkPersistentFlagRequired("portfolio")
-	removeCmd.PersistentFlags().StringVarP(&ticker, "ticker", "s", "", "Company name (required)")
-	removeCmd.MarkPersistentFlagRequired("ticker")
+	deleteCmd.PersistentFlags().StringVarP(&portfolio, "portfolio", "p", "", "Portfolio name (required)")
+	deleteCmd.MarkPersistentFlagRequired("portfolio")
+	deleteCmd.PersistentFlags().StringVarP(&ticker, "ticker", "s", "", "Company name (required)")
+	deleteCmd.MarkPersistentFlagRequired("ticker")
 
 	reportCmd.PersistentFlags().StringVarP(&today, "today", "t", "", "Portfolio name (required)")
 	reportCmd.PersistentFlags().IntVarP(&day, "day", "d", 1, "Report last given number of days ")

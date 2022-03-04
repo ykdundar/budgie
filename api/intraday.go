f47aaea84b8a34829091e7452e2528f7f27232da
Yesim Dundar <kuranyesim@gmail.com>
2022-03-04 20:56:06 +0100

    Add intraday API endpoint

---
+++ api/intraday.go
@@ -0,0 +1,49 @@
+package api
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+)
+
+type Intraday struct {
+	Data []struct {
+		Open     float64 `json:"open"`
+		High     float64 `json:"high"`
+		Low      float64 `json:"low"`
+		Last     float64 `json:"last"`
+		Close    float64 `json:"close"`
+		Volume   float64 `json:"volume"`
+		Date     string  `json:"date"`
+		Symbol   string  `json:"symbol"`
+		Exchange string  `json:"exchange"`
+	} `json:"data"`
+}
+
+// TODO: Symbols can be a slice of strings. The API supports comma seperated values
+func IntradayRequest(symbols string) (Intraday, error) {
+	const endpointURL string = "/intraday/latest"
+
+	baseURL := BaseURL()
+	values := baseURL.Query()
+
+	values.Add("symbols", symbols)
+
+	baseURL.RawQuery = values.Encode()
+	baseURL.Path = baseURL.Path + endpointURL
+
+	response, err := http.Get(baseURL.String())
+
+	if err != nil {
+		return Intraday{}, errors.New("the HTTP request has failed with an error")
+	} else {
+		data, _ := ioutil.ReadAll(response.Body)
+		intraday := Intraday{}
+		err := json.Unmarshal(data, &intraday)
+		if err != nil {
+			return Intraday{}, err
+		}
+		return intraday, nil
+	}
+}

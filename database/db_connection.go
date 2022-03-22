e4ef54210e8dc429b5bdd9d09ddd4fe544be9cac
Yesim Dundar <kuranyesim@gmail.com>
2022-03-22 23:50:49 +0100

    Save budgie.db to home directory as a hidden file

--- database/db_connection.go
+++ database/db_connection.go
@@ -1,6 +1,12 @@
 package database
 
-import "database/sql"
+import (
+	"database/sql"
+	"os"
+	"path/filepath"
+)
 
 // TODO: Save into home folder instead of repository
-var DBConnection, _ = sql.Open("sqlite3", "./budgie.db")
+
+var homeDirectory, _ = os.UserHomeDir()
+var DBConnection, _ = sql.Open("sqlite3", filepath.Join(homeDirectory, ".budgie.db"))

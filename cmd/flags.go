7e3faa4bcab19cac9a8e19029555d2661a1b68e4
Yesim Dundar <kuranyesim@gmail.com>
2022-03-23 17:55:19 +0100

    Add flag descriptions

--- cmd/flags.go
+++ cmd/flags.go
@@ -2,15 +2,14 @@ package cmd
 
 // flags
 var (
-	token     string // token represents an API token obtained from marketstack
-	name      string
-	currency  string
-	active    bool
-	rename    string
-	portfolio string
-	ticker    string
-	price     float64
-	shares    int
-	date      string
-	id        int
+	token     string  // token represents an API token obtained from marketstack
+	name      string  // name represents portfolio and stock names
+	currency  string  // currency represents portfolio currencies
+	rename    string  // rename represents the new name is to be given
+	portfolio string  // portfolio represents portfolio name belongs to a selected stock
+	ticker    string  // ticker represents stock symbols
+	price     float64 // price represents market value of selected stock
+	shares    int     // shares represents number of shares traded
+	date      string  // date represents the day of transaction
+	id        int     // id represents the order of stocks, portfolios and transactions
 )

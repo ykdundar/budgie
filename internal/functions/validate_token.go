4315528b90c6debe2416c1c13003d2e5e193c891
Yesim Dundar <kuranyesim@gmail.com>
2022-03-25 00:27:49 +0100

    Add tests to validateToken and delete dead code

--- internal/functions/validate_token.go
+++ internal/functions/validate_token.go
@@ -5,8 +5,8 @@ import "errors"
 // ValidateToken checks if a token is valid or not
 // it returns an error, if the given token is invalid
 func ValidateToken(token string) error {
-	if token == "" || len(token) != 32 {
-		return errors.New("Please enter a valid token!")
+	if len(token) != 32 {
+		return errors.New("please enter a valid token")
 	}
 
 	return nil
--- internal/functions/validate_token_test.go
+++ internal/functions/validate_token_test.go
@@ -1,1 +1,15 @@
 package functions
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestValidateToken(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Nil(ValidateToken("12345678901234567890123456789012"))
+	assert.NotNil(ValidateToken(""))
+	assert.NotNil(ValidateToken("123"))
+	assert.NotNil(ValidateToken("1234567890123456789012345678901212"))
+}

9b45c2b84849858cd4d178d89cd4fc310db13f77
Yesim Dundar <kuranyesim@gmail.com>
2022-03-29 22:15:18 +0200

    Fix Stock type

--- internal/objects/stock.go
+++ internal/objects/stock.go
@@ -1,15 +1,8 @@
 package objects
 
-// TODO: Do we even use this?!
-// Stock Type
+// Stock represents basic information about a stock
 type Stock struct {
-	stockId     int
-	name        string
-	ticker      string
-	buyDate     int
-	sellDate    int
-	buyPrice    int
-	sellPrice   int
-	portfolioId int
-	shares      int
+	StockId     int
+	Ticker      string
+	PortfolioId int
 }

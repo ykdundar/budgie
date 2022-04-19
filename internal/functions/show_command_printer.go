b80b66a22d3731d22ce295b5a685d14006e47ba1
Yesim Dundar <kuranyesim@gmail.com>
2022-04-20 00:52:10 +0200

    Fix date and remove shrink table

--- internal/functions/show_command_printer.go
+++ internal/functions/show_command_printer.go
@@ -11,13 +11,13 @@ func ShowCmdPrinter(intraday api.Intraday, header string) {
 	rowConfigAutoMerge := table.RowConfig{AutoMerge: true}
 	t := table.NewWriter()
 	t.SetOutputMirror(os.Stdout)
-	t.AppendHeader(table.Row{header, header, header, header, header, header, header, header, header}, rowConfigAutoMerge)
-	t.AppendHeader(table.Row{"SYMBOL", "OPEN", "HIGH", "LOW", "LAST", "CLOSE", "VOLUME", "DATE", "EXCHANGE"})
+	t.AppendHeader(table.Row{header, header, header, header, header, header, header, header}, rowConfigAutoMerge)
+	t.AppendHeader(table.Row{"SYMBOL", "OPEN", "HIGH", "LOW", "LAST", "CLOSE", "DATE", "EXCHANGE"})
 
 	for _, v := range intraday.Data {
-		formattedDate, _ := time.Parse("2006-01-02", v.Date)
+		formattedDate, _ := time.Parse("2006-01-02T15:04:05-0700", v.Date)
 		t.AppendRow(table.Row{v.Symbol, v.Open, v.High, v.Low, v.Last,
-			v.Close, v.Volume, formattedDate.Format("2006-01-02"), v.Exchange})
+			v.Close, formattedDate.Format("2006-01-02"), v.Exchange})
 		t.AppendSeparator()
 	}
 	t.SetStyle(table.StyleRounded)
